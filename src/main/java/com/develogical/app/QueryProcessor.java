648efcd6eaca7cde13e4f17bbaf795e4b4ed3b33
Byron Glover <byron.glover@newsignature.com>
2021-03-15 14:43:26 +0000

    Update jenkinsfile

--- src/main/java/com/develogical/app/QueryProcessor.java
+++ src/main/java/com/develogical/app/QueryProcessor.java
@@ -7,7 +7,7 @@ public class QueryProcessor {
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         }
-        if (query.toLowerCase().contains("shakespeare")) {
+        if (query.toLowerCase().contains("java")) {
             return "Java is a set of computer software and specifications developed by James Gosling at Sun Microsystems, " +
                     "which was later acquired by the Oracle Corporation, that provides a system for developing application software "+
                     "and deploying it in a cross-platform computing environment.";
