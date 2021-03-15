3645e544bfbf0f06e54ec4b436dd291f60b5a0b8
Byron <byron@bytemech.com>
2021-03-15 14:12:11 +0000

    Added additional return for QueryProcessor

--- src/main/java/com/develogical/app/QueryProcessor.java
+++ src/main/java/com/develogical/app/QueryProcessor.java
@@ -7,6 +7,11 @@ public class QueryProcessor {
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         }
+        if (query.toLowerCase().contains("shakespeare")) {
+            return "Java is a set of computer software and specifications developed by James Gosling at Sun Microsystems, " +
+                    "which was later acquired by the Oracle Corporation, that provides a system for developing application software "+
+                    "and deploying it in a cross-platform computing environment.";
+        }
         return "";
     }
 }
